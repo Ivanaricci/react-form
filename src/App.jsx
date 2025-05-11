e0b41cbb4b842ddd5b4d4293cd62a0317e0c8c36
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-11 11:01:22 +0200

    [UPD] Milestone1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import { useState } from 'react'
 
+const articoli = ["Titolo1", "Titolo2", "Titolo3", "titolo4", "Titolo5"]
+
 function App() {
+    return <>
+
+        <div className="container">
+            <h1>Articoli</h1>
+            <ul className="list-group">
+                {articoli.map((articolo, index) => (
+                    <li key={index} className="list-group-item">{articolo}</li>
+                ))}
+            </ul>
+        </div>
+
+    </>
 
 }
 
